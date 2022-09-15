d74be8469430d024726aecc60146e7bf758cba1b
Ashlē Mercer <103979087+ashlemercer@users.noreply.github.com>
2022-09-15 18:47:19 -0500

    First Commit.

---
+++ plots.js
@@ -0,0 +1,48 @@
+function init() {
+    var selector = d3.select("#selDataset");
+  
+    d3.json("samples.json").then((data) => {
+      console.log(data);
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+  })}
+  
+  init();
+
+  function optionChanged(newSample) {
+    console.log(newSample);
+  }
+
+  function optionChanged(newSample) {
+    buildMetadata(newSample);
+    buildCharts(newSample);
+  }
+
+  function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+      var metadata = data.metadata;
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      var result = resultArray[0];
+      var PANEL = d3.select("#sample-metadata");
+  
+      PANEL.html("");
+      PANEL.append("h6").text(result.location);
+    });
+  }
+
+  function demographic(id) {
+    let data = d3.json("samples.json").then(data => {
+        const metadata = data.metadata;
+        let demoPanel = d3.select('#sample-metadata')
+        demoPanel.html('');
+        let filteredData = metadata.filter(sampleName => sampleName.id == id)[0]
+        Object.entries(filteredData).forEach(([key, value]) => {
+            demoPanel.append("h6").text(`${key.toUpperCase()}: ${value}`);
+        });
+    });
+}
